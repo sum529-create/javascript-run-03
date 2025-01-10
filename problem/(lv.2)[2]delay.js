0ac9261ad78406775dce13add60fba517837071f
노수민 <nosumin29@gmail.com>
2025-01-10 17:33:33 +0900

    feat: solve problem 02_02

--- problem/(lv.2)[2]delay.js
+++ problem/(lv.2)[2]delay.js
@@ -9,7 +9,13 @@
  */
 
 // TODO: delay 함수를 작성하세요.
-async function delay(ms) {}
+async function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("done");
+    }, ms);
+  });
+}
 
 // export를 수정하지 마세요.
 export { delay };
